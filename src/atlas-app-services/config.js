c98ed7e8d6378525e50086077239cdb71a3c7b60
LJ <81748770+elle-j@users.noreply.github.com>
2023-03-31 05:13:10 +0200

    Revert "Add example code."
    This reverts commit 9b0d7e37e29b089d376441a22fb493fd2e073ea0.

--- src/atlas-app-services/config.js
+++
@@ -1,4 +0,0 @@
-const { BSON } = require('realm');
-
-exports.ATLAS_APP_ID = 'wawa-node-app-hngnk';
-exports.SYNC_STORE_ID = new BSON.ObjectId('6426106cb0ad9713140883ed');
--- src/logger.js
+++
@@ -1,9 +0,0 @@
-// This is meant to be replaced with a preferred logging implemention.
-exports.logger = {
-  info(message) {
-    console.info(new Date().toLocaleString(), '|', message);
-  },
-  error(message) {
-    console.error(new Date().toLocaleString(), '|', message);
-  },
-};
